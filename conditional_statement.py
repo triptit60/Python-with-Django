db29ba575b26b711a081f0c03656bc55c18b6ad4
Tripti Tiwari <triptitiwari625@gmail.com>
2025-01-20 07:52:19 +0545

    con-statement

---
+++ conditional_statement.py
@@ -0,0 +1,31 @@
+#day=input("Enter  a day")
+
+#if day=="wednesday": 
+    #2
+    # print("Xya aja ta padhna jana parxa")
+
+    #Conditional Statement
+    #if,elif,else
+     
+# 2.number=int(input("Enter a number"))
+
+#if number%2==0:
+   #print("Number is even")
+#else:
+    #print("Number is Odd")
+
+#number=int(input("enter a Number"))
+
+#if number>0:
+   # print("Enter Number is positive")
+#elif number==0:
+   #print("It is Zero")
+#else:
+    #print("Number is Negative")
+
+#n=int(input("Enter a number"))
+
+#if n%5==0 and n%3==0:
+    #print("Number is divisible by 5 and 3")
+#else:
+    #print("Number is not divisble by 5 and 3")
