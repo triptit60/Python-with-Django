3bc884f5b07d94e4c47f6fede9f9a43fc1cbaf28
Tripti Tiwari <triptitiwari625@gmail.com>
2025-01-20 08:53:45 +0545

    datatypes

---
+++ datatypes.py
@@ -0,0 +1,25 @@
+#primitive datatypes (*integer & float) same variable with diff values which have diff memory location
+a=10
+print(id(a))
+a=20
+print(id(a))
+
+#int,float,string,boolean(True&False)
+a=10
+print(type(a))
+ #float
+b=1.2
+print(type(b))
+
+#string
+name="tripti"
+print(type(name))
+
+name="""gcgd fhjh jhjd jh
+        jhjh jfhj hjfhj hjfh
+        jhgj jfjhj diu"""
+print(name)
+print(type(name))
+
+#reference datype
+#list,tuple,set,dictonary
