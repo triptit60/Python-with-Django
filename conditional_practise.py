79bb2011844f0980f2c5611959c95fbcd60ed457
Tripti Tiwari <triptitiwari625@gmail.com>
2025-01-20 07:26:21 +0545

    condition

---
+++ conditional_practise.py
@@ -0,0 +1,93 @@
+#1. write a program that asks the length or width of two rectangle, the program should tell the user
+#  which rectangle has the greater area or if the areas are the same.
+
+# l1=int(input("Enter length for first rectangle: "))
+# b1=int(input("Enter breadth for first rectaghle: "))
+# l2=int(input("Enter length for second rectangle: "))
+# b2=int(input("Enter breadth for second rectangle: "))
+# if l1*b1>l2*b2:
+#     print("First Rectangle has greater area")
+# elif l1*b1==l2*b2:
+#     print("Both Rectangle has same area")
+# else: 
+#     print("Second Rectangle has greater area")
+
+#2.Write a pragram to print roman number between 1 to 5 if greater than 5 show1 error(nested if)
+# n=int(input("Enter a number"))
+# if n==1:
+#   print("I")
+# if n==2:
+#        print("II")
+# if n==3:
+#    print("III")
+# if n==4:
+#           print("IV")
+# if n==5:
+#                 print("V")
+# else:
+#                 print("Error")
+#     else:
+#   print("Error")
+#  else: 
+#   print("Error")
+ 
+        
+         
+           
+# #3.nested if
+# #Divisible by 2 and 3 simultaneously
+# n=int(input("Enter a number"))
+# if n%2==0:
+#     print("Divisible by 2")
+#     if n%3==0:
+#         print("divisible by 3 ")
+#     else: 
+#        print("not divisible by 3")
+# else:
+#     print("Not sivisible by 2")
+ 
+#4.multiple elif
+# marks=int(input("Enter student marks"))
+# if marks>=90 and marks<=100:
+#    print("grade A")
+# elif marks>=80 and marks<=89:
+#    print("grade B")
+# elif marks>=70 and marks>=79:
+#          print("garde c")
+# elif marks>=60:
+#          print("garde F")
+# else: 
+#     print("fail")
+
+ #5.To check a password
+# Password= input("Enter a password")
+# if Password!="django":
+#     print("wrong password")
+# else:
+#     print("Your welcome")
+
+#6.vowel or constant
+vowel={"a","e","i","o","u"}
+letter= input("Enter a letter  ")
+if letter in vowel:
+    print("It is a vowel")
+else:
+    print("Letter is Constant")
+
+#7.
+#n=int(input("Enter a number"))
+
+#if n%5==0 and n%3==0:
+    #print("Number is divisible by 5 and 3")
+#else:
+    #print("Number is not divisble by 5 and 3")
+
+#8.
+# 2.number=int(input("Enter a number"))
+
+#if number%2==0:
+   #print("Number is even")
+#else:
+    #print("Number is Odd")
+
+#number=int(input("enter a Number"))
